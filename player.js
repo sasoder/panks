3a15a91c8617e2c1a69fad01c09d0a89d51b7b5b
Samuel Söderberg <samuelsoderberg99@gmail.com>
2020-02-22 23:38:49 +0100

    no longer spawn outside of map, update hp

--- player.js
+++ player.js
@@ -1,6 +1,6 @@
 // Player character
 
-const defaultShootAngle = 135 * Math.PI / 180 // 135 degrees
+const defaultShootAngle = 45 * Math.PI / 180 // 135 degrees
 console.log(defaultShootAngle)
 const defaultShootSpeed = 0.8
 
@@ -8,7 +8,7 @@ class Player {
     constructor() {
         this.width = 20
         this.height = 10
-        this.x = Math.floor((Math.random() * 9 / 10 * WIDTH) + WIDTH / 10)
+        this.x = Math.floor((Math.random() * 8 / 10 * WIDTH) + WIDTH / 10)
         this.y = 100
         this.velX = 0
         this.velY = 0
@@ -16,15 +16,28 @@ class Player {
         this.shootAngle = defaultShootAngle
         this.shootSpeed = defaultShootSpeed
         this.colour = colourList[Math.floor(Math.random() * colourList.length)]
+        this.name = 'Pierre'
     }
-    // apply gravity and velocity to player
     updatePlayer() {
+        if(this.hp <= 0) {
+            this.hp = 0
+        }
+        if(this.y >= HEIGHT) {
+            this.hp = 0
+        }
+    }
+    
+    applyGravity() {
+        // apply gravity and velocity to player
         this.velY += gravity
         this.y += this.velY
+
     }
 
     drawPlayer() {
-        ctx.fillStyle = this.colour
-        ctx.fillRect(this.x, this.y, this.width, this.height)
+        if(this.hp > 0) {
+            ctx.fillStyle = this.colour
+            ctx.fillRect(this.x, this.y, this.width, this.height)
+        }
     }
 }
