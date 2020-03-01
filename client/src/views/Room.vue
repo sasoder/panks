c6373c7f642d863badf6e0a497c4804f370707aa
Pierre Segerström <pise@kth.se>
2020-03-01 15:25:28 +0100

    Now possible to navigate to Room page

--- client/src/views/Room.vue
+++ client/src/views/Room.vue
@@ -1,11 +1,40 @@
 <template>
   <div>
-    <canvas id="gameCanvas"></canvas>
+    <p>Welcome to Room with ID: {{ this.room }}</p>
   </div>
 </template>
 
 <!-- We need to integrate keybutton input somehow... -->
-<script></script>
+<script>
+export default {
+  data() {
+    // ! - Can't use arrow notation because of the use of route?
+    return {
+      room: this.$route.params.roomID,
+      entries: [],
+      socket: null,
+    };
+  },
+  // TODO: Implement with sockets
+  created() {
+    this.socket = this.$root.socket;
+    /* this.socket.on('msg', (msg) => {
+      this.entries = [...this.entries, msg];
+    });
+    fetch(`/api/room/${this.room}/join`)
+      .then((resp) => {
+        if (!resp.ok) {
+          throw new Error(`Unexpected failure when joining room: ${this.room}`);
+        }
+        return resp.json();
+      })
+      .catch(console.error)
+      .then((data) => {
+        this.entries = data.list;
+      }); */
+  },
+};
+</script>
 
 <style scoped>
 </style>
--- client/src/views/RoomCard.vue
+++ client/src/views/RoomCard.vue
@@ -23,6 +23,7 @@ export default {
     // TODO: Implement joining a room
     joinRoom(roomID) {
         console.log(`Trying to join Room with ID: ${roomID}`);
+        this.$router.push(`/room/${roomID}`);
     },
     // Emitting event from child component
     // TODO: Add so that only creater of room can remove?
