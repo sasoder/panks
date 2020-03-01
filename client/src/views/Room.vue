b4969476fa1dc1a5722d90235d85d5cf317bb505
Pierre Segerström <pise@kth.se>
2020-03-01 15:28:10 +0100

    Base for socket setup in Room

--- client/src/views/Room.vue
+++ client/src/views/Room.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p>Welcome to Room with ID: {{ this.room }}</p>
+    <p>Welcome to Room with ID: {{ this.roomID }}</p>
   </div>
 </template>
 
@@ -10,28 +10,25 @@ export default {
   data() {
     // ! - Can't use arrow notation because of the use of route?
     return {
-      room: this.$route.params.roomID,
+      roomID: this.$route.params.roomID,
       entries: [],
       socket: null,
     };
   },
-  // TODO: Implement with sockets
+  // TODO: Set up join endpoint
   created() {
     this.socket = this.$root.socket;
-    /* this.socket.on('msg', (msg) => {
-      this.entries = [...this.entries, msg];
-    });
-    fetch(`/api/room/${this.room}/join`)
+    // TODO: Socket for fetching messages
+    // TODO: Socket for fetching game state
+    fetch(`/api/room/${this.roomID}/join`)
       .then((resp) => {
         if (!resp.ok) {
-          throw new Error(`Unexpected failure when joining room: ${this.room}`);
+          throw new Error(`Unexpected failure when joining room: ${this.roomID}`);
         }
-        return resp.json();
       })
-      .catch(console.error)
-      .then((data) => {
-        this.entries = data.list;
-      }); */
+      .catch((err) => {
+        console.error(err);
+      });
   },
 };
 </script>
