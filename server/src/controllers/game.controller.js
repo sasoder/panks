2447e50574647834e1d2e580394c4f959cd9d79a
Samuel Söderberg <sasoder@kth.se>
2020-03-06 17:11:54 +0100

    added fetch init gamestate endpoint

--- server/src/controllers/game.controller.js
+++ server/src/controllers/game.controller.js
@@ -20,4 +20,24 @@ router.post('/start', (req, res) => {
     model.startGame(roomID, width, height, amplitude);
 });
 
+
+// for getting the first gamestate when game is started
+router.post('/gameState/:roomID', (req, res) => {
+    const roomID = req.params.roomID;
+    
+    // Check if the room has an active game already
+    if (model.findRoom(roomID).game !== null) {
+        // Status: Forbidden
+        res.sendStatus(403);
+        return;
+    }
+
+    
+    // send gamestate to the client
+    const gameState = model.findRoom(roomID).game.gameState
+    res.status(200).json({
+        gameState
+    });
+});
+
 module.exports = { router };
