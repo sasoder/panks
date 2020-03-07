d345d399e622b9fe9659c71051827228a45efb27
Samuel Söderberg <sasoder@kth.se>
2020-03-07 19:07:12 +0100

    added updategame endpoint, fixed other endpoints

--- server/src/controllers/game.controller.js
+++ server/src/controllers/game.controller.js
@@ -5,30 +5,38 @@ const router = express.Router();
 
 router.post('/start', (req, res) => {
     const roomID = req.body.roomID;
-    const width = req.body.width;
-    const height = req.body.height;
-    const amplitude = req.body.amplitude;
-    
+    const room = model.findRoom(roomID);
     // Check if the room has an active game already
     if (model.findRoom(roomID).game !== null) {
         // Status: Forbidden
         res.sendStatus(403);
         return;
     }
+
+    const width = req.body.width;
+    const height = req.body.height;
+    const amplitude = req.body.amplitude;
+
+    // users is a list of the usernames, or rather their id's
+    const users = room.users
+    console.log('these are the users:', users)
+    
     
     // Start the game with given arguments
-    model.startGame(roomID, width, height, amplitude);
+    console.log('starting agme:')
+    model.startGame(roomID, width, height, amplitude, users);
 });
 
 
 // for getting the first gamestate when game is started
-router.post('/gameState/:roomID', (req, res) => {
+router.get('/gameState/:roomID', (req, res) => {
     const roomID = req.params.roomID;
+    console.log('roomid: ', roomID)
     
     // Check if the room has an active game already
-    if (model.findRoom(roomID).game !== null) {
-        // Status: Forbidden
-        res.sendStatus(403);
+    if (model.findRoom(roomID).game == null) {
+        // Status: not found
+        res.sendStatus(404);
         return;
     }
 
@@ -38,6 +46,24 @@ router.post('/gameState/:roomID', (req, res) => {
     res.status(200).json({
         gameState
     });
+});
+
+// for sending out gamestate updates from every player
+router.post('/updateGameState/:roomID', (req, res) => {
+    const roomID = req.params.roomID;
+    console.log('roomid: ', roomID)
+    
+    // Check if the room has an active game already
+    if (model.findRoom(roomID).game !== null) {
+        // Status: Forbidden
+        res.sendStatus(403);
+        return;
+    }
+
+    
+    // update gamestate
+    model.findRoom(roomID).game.gameState = req.body.gameState
+    res.sendStatus(200);
 });
 
 module.exports = { router };
